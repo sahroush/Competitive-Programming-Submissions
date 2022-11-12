f4a3a97046a1b6bd63d61cdd6f13b27258348eda
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:10:33 +0330

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc181_b/19088403.cpp
@@ -0,0 +1,39 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+ll n;
+ll ans = 0;
+
+int32_t main(){
+    migmig;
+	cin >> n;
+	for(int i = 1 ; i <= n ; i ++)
+		for(int j = i ; j <= n ; j += i)
+			ans += j;
+	cout << ans;
+    return(0);
+}
