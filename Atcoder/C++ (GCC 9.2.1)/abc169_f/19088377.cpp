69306beeaaab844f1e1441dfaadc00c7122a9078
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:10:55 +0330

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc169_f/19088377.cpp
@@ -0,0 +1,36 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+ll a ;
+string b;
+
+int32_t main(){
+	cin >> a >> b;
+	ll B = (b[0] - '0')*100 + (b[2] - '0')*10 + (b[3] - '0');
+	cout << ll((a * B) / 100);
+    return(0);
+}
