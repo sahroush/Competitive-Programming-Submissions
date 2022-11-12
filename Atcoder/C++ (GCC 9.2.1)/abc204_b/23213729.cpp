f629201b60dee7b02bd0ab3a22152e075fc7ea8a
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:08:56 +0330

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc204_b/23213729.cpp
@@ -0,0 +1,32 @@
+//叫んで 藻掻もがいて 瞼まぶたを腫らしても
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int , int> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int maxn = 3e6;
+const ll mod = 1e9+7;
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+int n;
+ll a[maxn] , ans;
+
+int32_t main(){
+	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+	cin >> n;
+  for(int i = 0 ; i < n ; i ++)cin >> a[i] , ans += max(0ll , a[i] - 10);
+  
+  	cout << ans;
+
+	return(0);
+}
