0e8c95fc8d74e0a290c663011110445785f5fedf
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:14:23 +0330

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc159_c/11457216.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+typedef pair<ll  ,ll > pll;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;//AmShZ
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ms(x , y) memset(x , y , sizeof x);
+ll pw(ll a, ll b, ll md = mod) {
+    ll res = 1;while(b){if(b&1){res=(a*res)%mod;}a=(a*a)%mod;b>>=1;}return(res);
+}
+
+int n  , m;
+
+int main(){
+    migmig
+    cin >> n >> m;
+    cout << n*(n - 1)/2 + m*(m - 1)/2;
+    return(0);
+}
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
+//kuxtal jach uts chuchul u u páajtal máak jach táaj meyajilo'ob le castigadas tumen ch'aik descansos.
