f9f9fe61ea3b821940bdc0cc9d71e8e685e49b42
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:11:45 +0330

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc091_a/17751685.cpp
@@ -0,0 +1,64 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ms(x , y) memset(x , y , sizeof x);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+const int K = 52;
+
+struct Mat{
+    ll m[K][K];
+    Mat(int diag = -1){
+        ms(m , 0);
+        if(diag==0)for(int i = 0 ; i < K ; i ++)m[i][i] = 1;
+        if(diag>0)for(int i = 0 ; i < diag ; i ++)m[i][i+1]=1;
+    }
+    Mat operator* (const Mat &b) const{
+        Mat c = Mat();
+        for(int i = 0 ; i < K ; i ++)
+            for(int k = 0 ; k < K ; k ++)
+                for(int j = 0 ; j < K ; j ++)
+                    c.m[i][j] = (ll(c.m[i][j]) + ll(m[i][k]) * b.m[k][j])%mod;
+        return(c);
+    }
+};
+Mat pw(Mat a, ll b){Mat res(0);while(b){if(b&1){res=(a*res);}a=(a*a);b>>=1;}return(res);}
+
+ll n , k;
+Mat a;
+
+int32_t main(){
+    migmig
+    cin >> n >> k;
+    for(int i = 0 ; i <n ; i ++)
+        for(int j = 0 ; j < n ; j ++)
+            cin >> a.m[i][j];
+    a = pw(a , k);
+    ll ans = 0;
+    for(int i = 0 ; i < n ; i ++ )
+        for(int j = 0 ; j < n ; j ++)
+            ans = (ans + a.m[i][j])%mod;
+    cout << ans;
+    return(0);
+}
