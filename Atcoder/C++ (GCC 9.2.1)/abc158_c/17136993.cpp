3eabff1a12f88133f50a1b67a6fbfb68d61a1b04
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:12:12 +0330

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc158_c/17136993.cpp
@@ -0,0 +1,42 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ms(x , y) memset(x , y , sizeof x);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+ll n , b , r;
+
+int32_t main(){
+    migmig
+	cin >> n >> b >> r;
+  	ll ans = n / (b + r) * b;
+  	n%=(b + r);
+  	ans += min(n ,b);
+    cout << ans;
+
+
+
+
+    return(0);
+}
