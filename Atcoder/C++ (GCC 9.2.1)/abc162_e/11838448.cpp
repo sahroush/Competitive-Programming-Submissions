a11b32a3b4160f2610101658d0c5cc2e80143492
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:13:52 +0330

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc162_e/11838448.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+ 
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ms(x , y) memset(x , y , sizeof x);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+ 
+ 
+ 
+int main(){
+    migmig
+ 	int n;
+  cin >> n;
+  ll sum = 0;
+  for(int i = 1 ; i <= n ; i ++) for(int j = 1 ; j <= n ; j ++) for(int k = 1 ; k <= n ; k ++) sum+=__gcd(i , __gcd(j , k));
+ 	cout << sum;
+    return(0);
+}
+ 
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
+//kuxtal jach uts chuchul u u páajtal máak jach táaj meyajilo'ob le castigadas tumen ch'aik descansos.
