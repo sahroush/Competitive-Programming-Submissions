c122edd5c1fbc5cf39c301c1efa2953cf893b0e6
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-13 00:15:57 +0330

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc143_b/8025359.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const ll maxn = 100;
+const ll ZERO = 0;
+const ll SADAT = 7.84e-17;
+const ll INF = 1e10;
+const ll mod = 1e9+7;
+
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+
+int a[maxn];
+int b[maxn];
+int n;
+
+int main(){
+	cin >> n;
+	for(int i = 0 ; i < n-1 ;i ++){
+		cin >> b[i];
+	}
+	fill (a, a+ n , 1e8);
+	for (int i = 0 ; i < n-1; i ++){
+		if (a[i] > b[i]){
+			a[i] = b[i];
+		}
+		if (a[i+1] > b[i]){
+			a[i+1] = b[i];
+		}
+	}
+	ll ans  = 0 ;
+	for (int  i = 0 ; i < n ; i ++){
+		ans += a[i];
+	}
+	cout << ans;
+
+	return(0);
+}
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, l is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
