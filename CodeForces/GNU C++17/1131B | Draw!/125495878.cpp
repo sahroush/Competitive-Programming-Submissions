f26b1fb885221529868376f26c20feffdb8e7924
soroush sahraei <soroush.sahraei@gmail.com>
2022-11-15 00:50:05 +0330

    Committing files

---
+++ CodeForces/GNU C++17/1131B | Draw!/125495878.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int , int> pii;
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+const int maxn = 3e6;
+const ll mod = 1e9+7;
+ 
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+ 
+int q;
+int a, b;
+int u, v;
+int ans = 1;
+ 
+int32_t main(){
+ cin.tie(0)->sync_with_stdio(0);
+ cin >> q;
+ while(q --){
+  cin >> u >> v;
+  int L = max(a , b), R = min(u, v);
+  ans += max(0 , R - L + (a != b));
+  a = u, b = v;
+ }
+ cout << ans;
+ return(0);
+}
